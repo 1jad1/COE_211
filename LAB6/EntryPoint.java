8f539106df299e952681a8f27f098a50bb841ea8
1jad1 <100035922+1jad1@users.noreply.github.com>
2022-03-15 22:02:56 +0200

    Add files via upload

--- LAB6/EntryPoint.java
+++ LAB6/EntryPoint.java
@@ -1,3 +1,7 @@
+/* For the first bonus exercise i think the code worked but not when i put 
+number 3 as an operator (maybe it is not the correct code) and as for the 
+second bonus exercise i was a bit lost on what to use ("if" or another "while"
+or "do while" if it works/ if i use "if" i didn't know how to re=execute the code) */
 import java.util.Scanner;
 
 public class EntryPoint {
@@ -8,6 +12,8 @@ public class EntryPoint {
 	Scanner scan=new Scanner(System.in);
 	int option;
 	
+	String Exit;
+	do{
 
 	System.out.println("Which service would you like to use?" 
 				+ "\n" + "[1]: Basic week visualizer" 
@@ -34,11 +40,22 @@ switch (option){
 	case 4:
 		Employee emp= new Employee();
 		System.out.print(emp.toString());
-		break;		
-}
-
+		break;
+	}
+	
 	
 
+System.out.print("Would you like to perform another operation? (y/n)");
+scan.nextLine();
+Exit=scan.nextLine();
 
-    }
+
+} while(Exit.charAt(0) != 'n');
+
+
+
+
+
+
+	}
 }
